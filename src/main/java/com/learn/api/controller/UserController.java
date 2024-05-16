c0eb61a215eaf9a81c536292ff4f648ccb45963c
dotdesign23 <61997056+dotdesign23@users.noreply.github.com>
2024-05-17 03:38:43 +0700

    Add CRUD

--- src/main/java/com/learn/api/controller/UserController.java
+++ src/main/java/com/learn/api/controller/UserController.java
@@ -1,11 +1,11 @@
 package com.learn.api.controller;
 
+import com.learn.api.exception.ResourceNotFoundException;
 import com.learn.api.model.User;
 import com.learn.api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
@@ -15,8 +15,37 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
     @GetMapping("/users")
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User", "id", id));
+    }
+
+    @PutMapping("/users/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        User user = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User", "id", id));
+
+        user.setName(userDetails.getName());
+        user.setEmail(userDetails.getEmail());
+
+        return userRepository.save(user);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User", "id", id));
+
+        userRepository.delete(user);
+
+        return ResponseEntity.ok().build();
     }
 }
